7f77bb5a584f458b42484a4455b16d6feaf6f06f
Kbouti <121819340+Kbouti@users.noreply.github.com>
2023-12-15 14:24:37 -0800

    Add pseudocode and link to freecodecamp

--- script.js
+++ script.js
@@ -1,31 +1,50 @@
-console.log(`Hello world!`)
+console.log(`Hello world!`);
 
 // The assignment is to create two either classes or factories (Me thinks I'm going with classes)
-// One class will represent the linkedList as a whole 
-// The other class will represent the individual nodes in the the linked List. This should contain a value property and a link to the nextNode. Instructions are to set both as null by default. 
+// One class will represent the linkedList as a whole
+// The other class will represent the individual nodes in the the linked List. This should contain a value property and a link to the nextNode. Instructions are to set both as null by default.
 
 // helpful review on classes, constructors, and factories:
 // https://www.youtube.com/watch?v=fbuyliXlDGI
 
-class LinkedList{
-    constructor(name, headNode){
-        this.name = name;
-        this.headNode = headNode;
+// The head node will at first have a value and reference of null.
+// When a new node is added, the reference to the last node becomes the new node, and the reference for the new node becomes null
+// This seems more helpful:
+// https://www.freecodecamp.org/news/how-linked-lists-work/
+
+
+class LinkedList {
+  constructor(name) {
+    this.name = name;
+    this.headNode = new Node(null, null);
+  }
+  append(value) {
+    // This function should create a new node and add it to the end of the linkedList
+    let newNode = new Node(value, null);
+
+    // I think maybe a while loop to start at the head and move through the list?
+    while(this.headNode.reference !== null){
+
     }
+
+  }
+  preprend(value){
+    // This function creates a new node and adds it to the beginning of the linkedList
+  }
 }
 
-class Node{
-    constructor(value, reference){
-        this.value = value;
-        this.reference = reference;
-    }
+class Node {
+  constructor(value, reference) {
+    this.value = value;
+    this.reference = reference;
+  }
 
-    log(){
-        console.log(this);
-        console.log(this.value);
-    }
+  log() {
+    console.log(this);
+    console.log(this.value);
+  }
 }
 
 let node1 = new Node(`fiddlesticks`, null);
 
-node1.log()
+node1.log();
